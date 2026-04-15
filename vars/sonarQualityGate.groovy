46bccd72dc6239dac5dfd2d9a813b273ce50b984
Ashir-Qayyum <muhammadashir2004@gmail.com>
2026-04-15 18:58:56 +0500

    Testing CI/CD - Build 10 Att 01

--- vars/sonarQualityGate.groovy
+++ vars/sonarQualityGate.groovy
@@ -1,16 +1,30 @@
+// def call(sonarHost, sonarToken, projectKey) {
+//     sh """
+//     sleep 60
+
+//     STATUS=\$(curl -s -u ${sonarToken}: \
+//     ${sonarHost}/api/qualitygates/project_status?projectKey=${projectKey} \
+//     | grep -o '"status":"OK"')
+
+//     echo "Quality Gate: \$STATUS"
+
+//     if [ -z "\$STATUS" ]; then
+//         echo "Quality Gate Failed"
+//         exit 1
+//     fi
+//     """
+// }
+
 def call(sonarHost, sonarToken, projectKey) {
     sh """
     sleep 60
 
     STATUS=\$(curl -s -u ${sonarToken}: \
-    ${sonarHost}/api/qualitygates/project_status?projectKey=${projectKey} \
-    | grep -o '"status":"OK"')
+    ${sonarHost}/api/qualitygates/project_status?projectKey=${projectKey})
 
-    echo "Quality Gate: \$STATUS"
-
-    if [ -z "\$STATUS" ]; then
+    echo "\$STATUS" | grep '"status":"OK"' || {
         echo "Quality Gate Failed"
         exit 1
-    fi
+    }
     """
 }
