b28b0dbaeac4b28374d5d16da027cb32b8be3064
Ashir-Qayyum <muhammadashir2004@gmail.com>
2026-04-16 13:31:35 +0500

    Testing CI/CD - Build 24 Att 01

--- vars/deployHelmViaSSH.groovy
+++ vars/deployHelmViaSSH.groovy
@@ -10,13 +10,14 @@
 //     """
 // }
 
+def call(user, server, key, image, release, chartPath) {
+        sh """
+        ssh -i ${key} ${user}@${server} "
+            export PATH=\\$PATH:/usr/local/bin
 
-sh """
-ssh -i ${key} ${user}@${server} "
-    export PATH=\\$PATH:/usr/local/bin
-
-    helm upgrade --install ${release} '${chartPath}' \
-    --set image.repository=ashirqayyum/asgn-09-java-app \
-    --set image.tag=latest
-"
-"""
+            helm upgrade --install ${release} '${chartPath}' \
+            --set image.repository=ashirqayyum/asgn-09-java-app \
+            --set image.tag=latest
+        "
+        """
+}
