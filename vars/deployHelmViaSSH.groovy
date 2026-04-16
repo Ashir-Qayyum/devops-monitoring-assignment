0cd1c3a0e4b3876f138340673e77487087646441
Ashir-Qayyum <muhammadashir2004@gmail.com>
2026-04-16 15:37:21 +0500

    Testing CI/CD for Docs - Build 25 Att 01

--- vars/deployHelmViaSSH.groovy
+++ vars/deployHelmViaSSH.groovy
@@ -1,14 +1,4 @@
-// def call(user, server, key, image, release, chartPath) {
-//     sh """
-//     ssh -i ${key} ${user}@${server} "
-//         export PATH=\\$PATH:/usr/local/bin
-        
 
-//         helm upgrade --install ${release} '${chartPath}' \
-//         --set image.repository=${image}
-//     "
-//     """
-// }
 
 def call(user, server, key, image, release, chartPath) {
         sh """
