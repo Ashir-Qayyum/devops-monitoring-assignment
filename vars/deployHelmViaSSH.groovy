d71dc8a110b9ec74cbdf3e7b4b94c0f01a37e593
Ashir-Qayyum <muhammadashir2004@gmail.com>
2026-04-16 11:05:49 +0500

    Testing CI/CD - Build 18 Att 01

--- vars/deployHelmViaSSH.groovy
+++ vars/deployHelmViaSSH.groovy
@@ -1,10 +1,10 @@
 def call(user, server, key, image, release, chartPath) {
     sh """
-    ssh -i ${key} ${user}@${server} '
+    ssh -i ${key} ${user}@${server} "
         export PATH=\\$PATH:/usr/local/bin
 
-        helm upgrade --install ${release} ${chartPath} \
+        helm upgrade --install ${release} '${chartPath}' \
         --set image.repository=${image}
-    '
+    "
     """
 }
